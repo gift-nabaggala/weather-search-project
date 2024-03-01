e1cf06eaa26b57fcd0066f37aba71ae9d68066e7
gift-nabaggala <nabaggalagift21@gmail.com>
2024-03-01 05:20:11 -0800

    Made the API integration, added windspeed and description

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayData(response) {
+  console.log(response);
+
+  let currentTime = document.querySelector("#time");
+  let date = new Date(response.data.dt * 1000);
+  currentTime.innerHTML = formatDate(date);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
 function searchCity(event) {
   event.preventDefault();
 
@@ -8,3 +42,8 @@ function searchCity(event) {
 let weatherSearchForm = document.querySelector("#weather-search-form");
 
 weatherSearchForm.addEventListener("submit", searchCity);
+
+let apiKey = "8914d369cdad4fdf805b6088475a669b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metrics`;
+
+axios.get(apiUrl).then(displayData);
